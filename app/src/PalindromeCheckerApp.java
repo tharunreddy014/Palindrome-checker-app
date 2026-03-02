4d556ec1fedf31ddda1cb0c51c40c9d466016a42
tharunreddy014 <ammireddytharunkumarreddy@gmail.com>
2026-03-02 14:44:32 +0530

    uc1 commited

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        // Application Entry & Welcome Message
+        System.out.println("=== Palindrome Checker Management System ===");
+        System.out.println("Application Version: 1.0");
+        System.out.println("Status: Startup flow established.");
+    }
+}
