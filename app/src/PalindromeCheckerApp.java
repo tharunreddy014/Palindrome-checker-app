4bf6651f6945437cab23c4287939f3ec10c4261c
tharunreddy014 <ammireddytharunkumarreddy@gmail.com>
2026-03-02 14:58:55 +0530

    uc2 commited

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,9 +1,22 @@
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-        // Application Entry & Welcome Message
-        System.out.println("=== Palindrome Checker Management System ===");
-        System.out.println("Application Version: 1.0");
-        System.out.println("Status: Startup flow established.");
+
+        String input = "madam";
+        String original = input;
+        String reverse = "";
+
+
+        int length = input.length();
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + input.charAt(i);
+        }
+
+
+        System.out.println("Hardcoded String: " + input);
+        if (original.equals(reverse)) {
+            System.out.println("Result: The string is a palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
     }
 }
